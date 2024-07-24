d4bff4bdc28ea56442a0817c0b06393276ee7fbe
Kazuki Yamada <koukun0120@gmail.com>
2024-07-24 23:26:56 +0900

    feat(file): Improve detect binary file

--- src/utils/fileHandler.ts
+++ src/utils/fileHandler.ts
@@ -1,52 +1,43 @@
 import * as fs from 'fs/promises';
-import isBinaryPath from 'is-binary-path';
+import { isBinary } from 'istextorbinary';
 import jschardet from 'jschardet';
 import iconv from 'iconv-lite';
 import { RepopackConfigMerged } from '../types/index.js';
 import { getFileManipulator } from './fileManipulator.js';
+import { logger } from './logger.js';
 
 export async function processFile(
   filePath: string,
   config: RepopackConfigMerged,
   fsModule = fs,
 ): Promise<string | null> {
-  // Skip binary files
-  if (isBinaryPath(filePath)) {
+  if (isBinary(filePath)) {
+    logger.debug(`Skipping binary by path. path: ${filePath}`);
     return null;
   }
 
-  try {
-    const buffer = await fsModule.readFile(filePath);
-    const encoding = jschardet.detect(buffer).encoding || 'utf-8';
-    let content = iconv.decode(buffer, encoding);
+  logger.trace(`Processing file: ${filePath}`);
 
-    if (!isValidTextContent(content)) {
-      return null;
-    }
+  const buffer = await fsModule.readFile(filePath);
 
-    content = preprocessContent(content);
-
-    if (config.output.removeComments) {
-      const manipulator = getFileManipulator(filePath);
-      if (manipulator) {
-        content = manipulator.removeComments(content);
-      }
-    }
-
-    return content;
-  } catch (error) {
-    console.warn(`Error processing file ${filePath}:`, error);
+  if (isBinary(null, buffer)) {
+    logger.debug(`Skipping binary by content. path: ${filePath}`);
     return null;
   }
-}
 
-function isValidTextContent(content: string): boolean {
-  // Check the validity of the text
-  // If the percentage of non-printable characters is greater than a certain value, it is judged not to
-  const nonPrintableChars = content.replace(/[\x20-\x7E\n\r\t]/g, '');
+  const encoding = jschardet.detect(buffer).encoding || 'utf-8';
+  let content = iconv.decode(buffer, encoding);
 
-  // If the percentage of non-printable characters is greater than 10%, it is judged not to be text
-  return nonPrintableChars.length / content.length < 0.1;
+  content = preprocessContent(content);
+
+  if (config.output.removeComments) {
+    const manipulator = getFileManipulator(filePath);
+    if (manipulator) {
+      content = manipulator.removeComments(content);
+    }
+  }
+
+  return content;
 }
 
 export function preprocessContent(content: string): string {
